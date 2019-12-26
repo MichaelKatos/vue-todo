e1fbaac5ee07884f0379baa4940d76ec17b7ee2a
Michael Katos <katoskreations@gmail.com>
2019-12-26 16:26:47 -0500

    Brad Traversy Vue crash course

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <form>
+      <input type="text" name="title" placeholder="Add Todo..." />
+      <input type="submit" value="+" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo"
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 0.8rem;
+  line-height: 2.2rem;
+  border: none;
+  box-shadow: 2px 1px 2px rgba(0, 0, 0, 0.2);
+  font-size: 1.5rem;
+}
+
+input[type="submit"] {
+  flex: 1;
+  padding: 0.8rem;
+  line-height: 2rem;
+  box-shadow: 2px 1px 2px rgba(0, 0, 0, 0.2);
+}
+
+.btn {
+  border: none;
+  background: green;
+  color: #fff;
+  font-size: 3rem;
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <h1>Todo List</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 1rem;
+  line-height: 3rem;
+}
+
+.header h1 {
+  font-size: 2rem;
+}
+.header a {
+  color: #ffffff;
+  padding-right: 0.5rem;
+}
+</style>
